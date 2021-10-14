a6ad4d5a7af9f7a025142ce689be5a713afddf24
Guksu <yung3152@daum.net>
2021-10-14 16:19:54 +0900

    final

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -5,11 +5,11 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
-import Detail from "Routes/Detail/index";
-import Home from "Routes/Home/index";
-import Search from "Routes/Search/index";
-import Tv from "Routes/Tv/index";
-import Header from "./Header";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Header from "Components/Header";
+import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 //일치되는 라우터가 없는 경우 redirect로 이동
 // switch 가 없는 경우 /tv/act 라는 페이지로 이동시 /tv 와 /tv/act가 동시에 렌더링되는 문제가 발생한다.
@@ -18,7 +18,7 @@ export default () => (
     <Header></Header>
     <Switch>
       <Route path="/" exact component={Home}></Route>
-      <Route path="/tv" exact component={Tv}></Route>
+      <Route path="/tv" exact component={TV}></Route>
       <Route path="/search" component={Search}></Route>
       <Route path="/movie/:id" component={Detail}></Route>
       <Route path="/show/:id" component={Detail}></Route>
